b3668128a48ca71f86ccb50c63110fdcba8d6ad2
Chaitanya Rathpd <rathodchaitanya39@gmail.com>
2026-02-15 13:04:03 +0530

    add crud to the table

--- services/urls.py
+++ services/urls.py
@@ -2,6 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("Service/",views.services),
-    path("ServicesTable/",views.ServicesTable),
+    path("Service/",views.services,name="Service"),
+    path("ServicesTable/",views.ServicesTable,name="ServicesTable"),
+    path("ServiceFilter/",views.ServiceFilter,name="ServiceFilter"),
+    path("ServiceDelete/<int:id>",views.ServiceDelete,name="ServiceDelete"),
+    path("ServiceUpdate/<int:id>",views.ServiceUpdate,name='ServiceUpdate'),
+    path("SortServcies/,<int:id>",views.SortService,name="SortServices")
 ]
--- services/views.py
+++ services/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from .models import Services
 from .forms import ServicesForm
 # Create your views here.
@@ -6,7 +6,7 @@ def services(request):
     if request.method =="POST":
         form = ServicesForm(request.POST)
         form.save()
-        return HttpResponse("service entered")
+        return redirect("ServicesTable")
     else:
         form = ServicesForm()
         return render(request,"services/services.html",{"form":form})
@@ -15,3 +15,35 @@ def services(request):
 def ServicesTable(request):
     services = Services.objects.all().values()
     return render(request,"services/servicestable.html",{"services":services})
+
+def ServiceFilter(request):
+    services = Services.objects.filter(Price__gt = 2500)
+    return render(request,"services/servicefilter.html",{"services":services})
+
+
+def ServiceDelete(request,id):
+    Services.objects.filter(id=id).delete()
+    return redirect("ServicesTable")
+
+def ServiceUpdate(request,id):
+     #database existing user... id -->
+    services = Services.objects.get(id=id) #select * from employee where id = 1
+    
+    if request.method == "POST":
+        form = ServicesForm(request.POST,instance=services)
+        form.save()
+        return redirect("ServicesTable")
+    else:
+        form = ServicesForm(instance=services)    
+        return render(request,"services/servicesupdate.html",{"form":form})
+
+
+def SortService(request,id):
+    if id == 1:
+        services = Services.objects.order_by("id")
+    elif id == 2:    
+        services = Services.objects.order_by("-id")
+    else:
+        services = Services.objects.all()    
+
+    return render(request,"services/servicestable.html",{"services":services})    
